c6ea99c31400850e94eab1ccc4e9841b61df007d
masayahh <84401294+masayahh@users.noreply.github.com>
2025-09-29 08:28:42 +0000

    test: make jest setup resilient (optional RNGH/reanimated)

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,6 @@
-require('react-native-gesture-handler/jestSetup');
+/**
+ * Minimal Jest setup for this repo.
+ * Some RN libs are optional here; guard them with try/catch.
+ */
+try { require('react-native-gesture-handler/jestSetup'); } catch (e) { /* optional */ }
+try { jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock')); } catch (e) { /* optional */ }
